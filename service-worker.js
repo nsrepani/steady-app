5de8c47301a61e591c6b39042f832caf07aa8a68
nsrepani <nomer.repani@gmail.com>
2026-07-03 16:22:29 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'steady-v1';
+const CACHE_NAME = 'steady-v2';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
